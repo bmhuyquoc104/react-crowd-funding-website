a1648265058c58e6e0118757285fd97d758cbebd
Võ Quốc Huy <56634994+bmhuyquoc104@users.noreply.github.com>
2022-03-17 15:47:00 +0700

    1. Set up project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {grommet, Grommet} from 'grommet'
+import imagesResource from './assets/images'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet className="App">
+      <img src = {imagesResource.CheckIcon} alt = "Test icon"/>
+    </Grommet>
   );
 }
 
---
+++ src/assets/images.js
@@ -0,0 +1,15 @@
+import BookmarkIcon from './images/icon-bookmark.svg'
+import CheckIcon from './images/icon-check.svg'
+import CloseMenuIcon from './images/icon-close-menu.svg'
+import CloseModalIcon from './images/icon-close-modal.svg'
+import HamburgerIcon from './images/icon-hamburger.svg'
+import HeroDesktop from './images/image-hero-desktop.jpg'
+import HeroMobile from './images/image-hero-mobile.jpg'
+import Logo from './images/logo.svg'
+import LogoMastercraft from './images/logo-mastercraft.svg'
+
+const imagesResource = {
+    BookmarkIcon,CheckIcon,CloseMenuIcon,CloseModalIcon,HamburgerIcon,HeroDesktop,HeroMobile,Logo,LogoMastercraft
+};
+
+export default imagesResource;
